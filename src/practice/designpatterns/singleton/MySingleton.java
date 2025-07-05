73ebaaaf67c891ae4daf346284cc02f8d980f86b
samparn <samparnpatra9@gmail.com>
2025-07-05 13:31:27 +0530

    More Practice

--- src/practice/designpatterns/singleton/MySingleton.java
+++ src/practice/designpatterns/singleton/MySingleton.java
@@ -1,13 +1,15 @@
 package practice.designpatterns.singleton;
 
+//Bill Pugh Singleton
 public class MySingleton {
     private MySingleton() {
     }
-
+    private static class Holder {
     private static final MySingleton MY_INSTANCE = new MySingleton();
+    }
 
     public static MySingleton getInstance() {
-        return MY_INSTANCE;
+        return Holder.MY_INSTANCE;
     }
 
     public void showMessage() {
